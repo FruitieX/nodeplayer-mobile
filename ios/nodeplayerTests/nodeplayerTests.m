2e46ede89030e9ff36a237ed49419fd5ae95997e
Rasmus Eskola <fruitiex@gmail.com>
2016-11-05 15:33:48 +0200

    Rename template to nodeplayer

--- ios/nodeplayerTests/nodeplayerTests.m
+++ ios/nodeplayerTests/nodeplayerTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface nodeplayerTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation nodeplayerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
